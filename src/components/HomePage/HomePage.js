29dbc8ccd3b989336c6f92d6da5e7182047683d1
GIAS UDDIN <gias.swe.ynu@gmail.com>
2021-03-21 12:48:58 +0800

    styele added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,46 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./HomePage.css";
-import image1 from "../../image/Frame.png";
-import image2 from "../../image/Frame-1.png";
-import image3 from "../../image/Frame-2.png";
-import image4 from "../../image/Group.png";
+import data from "../../FakeData/homeFakeData.json";
 import { Link } from "react-router-dom";
 const HomePage = () => {
-  const bus = "bus";
-  const train = "train";
-  const car = "car";
-  const bike = "bike";
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    setProduct(data);
+  }, []);
+  console.log(product);
   return (
     <div className="home-page container">
       <div className="row">
-        <div className="col-lg-3 col-md-6 image-link">
-          <div className="main-image">
-            <Link to={`/destination/${bike}`}>
-              <img src={image1} alt="" />
-            </Link>
+        {product.map((pd) => (
+          <div className="col-lg-3 col-md-6 image-link">
+            <div className="main-image">
+              <Link to={`/destination/${pd.name}`}>
+                <img src={pd.photoURl} alt="" />
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="col-lg-3 col-md-6 image-link">
-          <div className="main-image">
-            <Link to={`/destination/${bus}`}>
-              <img src={image2} alt="" />
-            </Link>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 image-link">
-          <div className="main-image">
-            <Link to={`/destination/${car}`}>
-              <img src={image3} alt="" />
-            </Link>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 image-link">
-          <div className="main-image">
-            <Link to={`/destination/${train}`}>
-              <img src={image4} alt="" />
-            </Link>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
